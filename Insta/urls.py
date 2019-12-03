8990d4a8c2543123cbc98b6abef172b0f0a05e39
hessdrexel <hshi2001us@hotmail.com>
2019-12-03 16:41:46 -0500

    display static page

--- Insta/urls.py
+++ Insta/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from Insta.views import HelloWorld
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', HelloWorld.as_view(), name='helloworld')
 
-    path('Insta/', include('Insta.urls')),
 ]
